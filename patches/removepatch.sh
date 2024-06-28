3c3e4cf99a5b07eecca30719cd432ec38aad97d9
Yervant <71306082+Yervant7@users.noreply.github.com>
2024-06-28 17:12:55 -0300

    Update removepatch.sh

--- patches/removepatch.sh
+++ patches/removepatch.sh
@@ -6,43 +6,73 @@ patch_files=(
     fs/read_write.c
     fs/stat.c
     drivers/input/input.c
+    drivers/Kconfig
+    drivers/Makefile
 )
 
 for i in "${patch_files[@]}"; do
-
-    if grep -q "ksu" "$i"; then
-        echo "Warning: $i contains KernelSU"
-        continue
-    fi
 
     case $i in
 
     # fs/ changes
     ## exec.c
     fs/exec.c)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
         sed -i '/#ifdef CONFIG_KSU/,/#endif/d' fs/exec.c
         ;;
 
     ## open.c
     fs/open.c)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
         sed -i '/#ifdef CONFIG_KSU/,/#endif/d' fs/open.c
         ;;
 
     ## read_write.c
     fs/read_write.c)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
         sed -i '/#ifdef CONFIG_KSU/,/#endif/d' fs/read_write.c
         ;;
 
     ## stat.c
     fs/stat.c)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
         sed -i '/#ifdef CONFIG_KSU/,/#endif/d' fs/stat.c
         ;;
 
-    # drivers/input changes
+    # drivers/ changes
     ## input.c
     drivers/input/input.c)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
         sed -i '/#ifdef CONFIG_KSU/,/#endif/d' drivers/input/input.c
         ;;
+
+    ## Kconfig
+    drivers/Kconfig)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
+        sed -i '/menu "KernelSU"/,/endmenu/d' drivers/Kconfig
+        ;;
+
+    ## Makefile
+    drivers/Makefile)
+        if grep -q "ksu" "$i"; then
+            echo "Warning: $i contains KernelSU"
+        fi
+        sed -i '/obj-$(CONFIG_KSU)[[:space:]]\+= ksu\//d' drivers/Makefile
+        sed -i '/obj-$(CONFIG_KSU)[[:space:]]\+= kernelsu\//d' drivers/Makefile
+        ;;
+        
     esac
 
 done
